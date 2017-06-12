6dee6c067423af578fd1b547198e01fe8e94173d
Geon <rndrjs123@naver.com>
2017-06-12 14:01:13 +0900

    Add FileEncrypt/Decrypt function

---
+++ app/src/main/java/com/example/denky/ageis/BufferSizeSettingException.java
@@ -0,0 +1,12 @@
+package com.example.denky.ageis;
+
+/**
+ * Created by Windows10 on 2017-06-12.
+ */
+
+public class BufferSizeSettingException extends Exception{
+    public BufferSizeSettingException(){
+        super("Buffer Size is invalid");
+    }
+}
+
